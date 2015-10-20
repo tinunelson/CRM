ff1139350f1deb2e16ee6f5b6bd0149680f982e0
tinunelson <tinunelson@gmail.com>
2015-10-20 09:29:15 +0530

    New Updates
    New Updates

---
+++ Samples/ConvertLeadNoContact/ConvertLeadNoContact/PluginStopConvertContact.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using System;
+using System.Collections.Generic;
+
+namespace ConvertLeadNoContact
+{
+    public class PluginStopConvertContact : IPlugin
+    {
+        #region Secure/Unsecure Configuration Setup
+        private string _secureConfig = null;
+        private string _unsecureConfig = null;
+
+        public PluginStopConvertContact(string unsecureConfig, string secureConfig)
+        {
+            _secureConfig = secureConfig;
+            _unsecureConfig = unsecureConfig;
+        }
+        #endregion
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            ITracingService tracer = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+            IPluginExecutionContext context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+            IOrganizationServiceFactory factory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+            IOrganizationService service = factory.CreateOrganizationService(context.UserId);
+
+            try
+            {
+
+
+                EntityReference leadid = (EntityReference)context.InputParameters["LeadId"];
+                ColumnSet attributes2 = new ColumnSet(new string[] { "subject"});
+                Entity lead = service.Retrieve(leadid.LogicalName, leadid.Id, attributes2);
+                //throw new InvalidPluginExecutionException(lead.Attributes["subject"].ToString());
+
+                context.InputParameters["CreateContact"] = false;
+
+                //if (context.InputParameters.Contains("Target"))
+                //{
+                //    Entity entity = (Entity)context.InputParameters["Target"];
+                //}
+
+                //else
+                //{
+                //    throw new InvalidPluginExecutionException("Hi");
+                //}
+
+                //TODO: Do stuff
+
+                //if (entity.Attributes["mobilephone"].ToString()=="" || entity.Attributes["mobilephone"]==null)
+                //{}
+                    //context.InputParameters["CreateContact"] = false;
+                   
+                
+
+            }
+            catch (Exception e)
+            {
+                throw new InvalidPluginExecutionException(e.Message);
+            }
+        }
+    }
+}
---
+++ Samples/Workflow/WorkflowTemperaturromCity/WorkflowTemperaturromCity/WorkFlowGetTemp.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Workflow;
+using System;
+using System.Activities;
+using System.IO;
+using System.Xml;
+
+namespace WorkflowTemperaturromCity
+{
+    public class WorkFlowGetTemp : CodeActivity
+    {
+        [Input("City")]
+        [RequiredArgument]
+        public InArgument<string> City { get; set; }
+
+        [Input("Country")]
+        [RequiredArgument]
+        public InArgument<string> Country { get; set; }
+
+        //[Output("Temp_F")]
+        //public OutArgument<string> Temp_F { get; set; }
+
+        [Output("Temp_C")]
+        public OutArgument<string> Temp_C { get; set; }
+
+
+        protected override void Execute(CodeActivityContext executionContext)
+        {
+            ITracingService tracer = executionContext.GetExtension<ITracingService>();
+            IWorkflowContext context = executionContext.GetExtension<IWorkflowContext>();
+            IOrganizationServiceFactory serviceFactory = executionContext.GetExtension<IOrganizationServiceFactory>();
+            IOrganizationService service = serviceFactory.CreateOrganizationService(context.UserId);
+
+            try
+            {
+                //Entity entity = (Entity)context.InputParameters["Target"];
+
+                //TODO: Do stuff
+
+                string city = City.Get(executionContext);
+                string country = Country.Get(executionContext);
+                Weather.GlobalWeather Client = new Weather.GlobalWeather();
+                string Get = Client.GetWeather(city,country);
+
+
+
+
+
+
+
+               
+
+                String xmlString = Get;
+
+                // Create an XmlReader
+                using (XmlReader reader = XmlReader.Create(new StringReader(xmlString)))
+                {
+                    //reader.ReadStartElement("CurrentWeather");
+
+                    while (reader.Read())
+                    {
+                        switch (reader.Name.ToString())
+                        {
+                            case "Temperature":
+                                string Temp=reader.ReadString();
+                                Temp_C.Set(executionContext, Temp);
+                                break;
+
+                        }
+
+
+                        
+                        
+                    }
+                   
+                    
+                  
+                }
+
+               
+
+
+
+
+
+            }
+            catch (Exception e)
+            {
+                throw new InvalidPluginExecutionException(e.Message);
+            }
+        }
+    }
+}
